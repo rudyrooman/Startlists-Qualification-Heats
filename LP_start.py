ecf411eea67e06128b69f44312617a66cea2fc1c
Rudy <rudy.rooman@gmail.com>
2023-03-13 21:22:11 +0100

    Add files via upload

--- LP_start.py
+++ LP_start.py
@@ -201,8 +201,6 @@ start_time = timeit.default_timer()
 wb = op.load_workbook(filename='LP_start_entries.xlsx')
 sheet1 = wb.active
 
-last_row = len(list(sheet1.rows))
-
 # read entered runners data file
 for teller in range(5, sheet1.max_row + 1):
     # check if row would be empty and leave for loop
